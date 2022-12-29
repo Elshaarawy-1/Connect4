58b05b4d1c527cf0bfdd3da78a2cf45adb27e3a1
Ahmed Ayman <ahmedayman4a77@gmail.com>
2022-12-29 16:25:02 +0200

    Use board int* and remove Move parameter

--- undo_redo.c
+++ undo_redo.c
@@ -1,30 +1,34 @@
+#include <stdio.h>
+#include <limits.h>
 #include "undo_redo.h"
 
-void undo_game(struct Stack* undostack, struct Stack* redostack, Configuration config, int board[][config.width], Player *player, Move *played_move)
+void undo_game(struct Stack* undostack, struct Stack* redostack, Configuration config, int* board, Player *player)
 {
-    played_move->column = pop(undostack);
-    if (played_move->column == INT_MIN)
+    Move played_move;
+    played_move.column = pop(undostack);
+    if (played_move.column == INT_MIN)
     {
         printf("You cannot undo.\n");
     }
     else
     {
-    unplay_move(config, board, player, played_move);
-    push(redostack, played_move->column);
+    unplay_move(config, board, player, &played_move);
+    push(redostack, played_move.column);
     }
 }
 
-void redo_game(struct Stack* undostack, struct Stack* redostack, Configuration config, int board[][config.width], Player *player, Move *played_move)
+void redo_game(struct Stack* undostack, struct Stack* redostack, Configuration config, int* board, Player *player)
 {
-    played_move->column = pop(redostack);
-    if (played_move->column == INT_MIN)
+    Move played_move;
+    played_move.column = pop(redostack);
+    if (played_move.column == INT_MIN)
     {
         printf("You cannot redo.\n");
     }
     else
     {
-        play_move(config, board, player, played_move);
-        push(undostack, played_move->column);
+        play_move(config, board, player, &played_move);
+        push(undostack, played_move.column);
     }
 }
 
--- undo_redo.h
+++ undo_redo.h
@@ -10,19 +10,13 @@
 /// @param config Configuration containing board width and height
 /// @param board The array representing the last board state. Gets updated with the played_move if the move is valid.
 /// @param player Information about the player including player id and score. Gets updated with the new score if the move is valid.
-/// @param played_move the move played where played_move.column is the column at which the move is played. 
-/// Gets updated with the move row if the move is valid.
-/// @return UNAVAILABLE_OPERATION.
-void undo_game(struct Stack* undostack, struct Stack* redostack, Configuration config, int board[][config.width], Player *player, Move *played_move);
+void undo_game(struct Stack* undostack, struct Stack* redostack, Configuration config, int * board, Player *player);
 
 /// @brief Redo last move if valid.
 /// @param config Configuration containing board width and height
 /// @param board The array representing the last board state. Gets updated with the played_move if the move is valid.
 /// @param player Information about the player including player id and score. Gets updated with the new score if the move is valid.
-/// @param played_move the move played where played_move.column is the column at which the move is played. 
-/// Gets updated with the move row if the move is valid.
-/// @return UNAVAILABLE_OPERATION
-void redo_game(struct Stack* undostack, struct Stack* redostack, Configuration config, int board[][config.width], Player *player, Move *played_move);
+void redo_game(struct Stack* undostack, struct Stack* redostack, Configuration config, int * board, Player *player);
 
 
 #endif
