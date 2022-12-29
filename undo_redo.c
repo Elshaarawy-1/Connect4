1628d98a5c3e5096407bb35f37d476326ab12828
Ahmed Ayman <ahmedayman4a77@gmail.com>
2022-12-29 22:44:38 +0200

    Change undo and redo to change game_state

--- undo_redo.c
+++ undo_redo.c
@@ -1,35 +1,50 @@
 #include <stdio.h>
 #include <limits.h>
+#include "tui.h"
 #include "undo_redo.h"
 
-void undo_game(struct Stack* undostack, struct Stack* redostack, Configuration config, int* board, Player *player)
+bool undo_game(GameState *game_state, struct Stack *undostack, struct Stack *redostack)
 {
     Move played_move;
     played_move.column = pop(undostack);
     if (played_move.column == INT_MIN)
     {
-        printf("You cannot undo.\n");
+        return false;
     }
     else
     {
-    unplay_move(config, board, player, &played_move);
-    push(redostack, played_move.column);
+        change_turn(game_state); // get the previous move's player
+        Player *current_player = get_current_player(game_state);
+        unplay_move(game_state, &played_move);
+        get_elapsed_time(game_state->timer_start, game_state->elapsed_time);
+        game_state->total_moves--;
+        current_player->number_of_moves--;
+
+        print_game_state(*game_state);
+        push(redostack, played_move.column);
+        return true;
     }
 }
 
-void redo_game(struct Stack* undostack, struct Stack* redostack, Configuration config, int* board, Player *player)
+bool redo_game(GameState *game_state, Stack *undostack, struct Stack *redostack)
 {
     Move played_move;
     played_move.column = pop(redostack);
     if (played_move.column == INT_MIN)
     {
-        printf("You cannot redo.\n");
+        return false;
     }
     else
     {
-        play_move(config, board, player, &played_move);
+        Player *current_player = get_current_player(game_state);
+        play_move(game_state, &played_move);
+        change_turn(game_state);
+        get_elapsed_time(game_state->timer_start, game_state->elapsed_time);
+        game_state->total_moves++;
+        current_player->number_of_moves++;
+
+        print_game_state(*game_state);
         push(undostack, played_move.column);
+        return true;
     }
 }
-
-
--- undo_redo.h
+++ undo_redo.h
@@ -1,22 +1,26 @@
 #ifndef UNDO_REDO_H
 #define UNDO_REDO_H
+#include "stdbool.h"
 #include "move_result.h"
 #include "fstack.h"
 #include "configuration.h"
 #include "player.h"
+#include "game_state.h"
 
 
 /// @brief Undo last move if valid.
 /// @param config Configuration containing board width and height
 /// @param board The array representing the last board state. Gets updated with the played_move if the move is valid.
 /// @param player Information about the player including player id and score. Gets updated with the new score if the move is valid.
-void undo_game(struct Stack* undostack, struct Stack* redostack, Configuration config, int * board, Player *player);
+
+bool undo_game(GameState *game_state, struct Stack *undostack, struct Stack *redostack);
 
 /// @brief Redo last move if valid.
 /// @param config Configuration containing board width and height
 /// @param board The array representing the last board state. Gets updated with the played_move if the move is valid.
 /// @param player Information about the player including player id and score. Gets updated with the new score if the move is valid.
-void redo_game(struct Stack* undostack, struct Stack* redostack, Configuration config, int * board, Player *player);
+
+bool redo_game(GameState *game_state, Stack *undostack, struct Stack *redostack);
 
 
 #endif
