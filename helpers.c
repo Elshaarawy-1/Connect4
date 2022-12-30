f94a6a25ef29376a77813cfb2759ee1bf9d1306a
Ahmed Ayman <ahmedayman4a77@gmail.com>
2022-12-30 21:17:26 +0200

    Add string and int helper functions

---
+++ helpers.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <limits.h>
+#include <ctype.h>
+#include "helpers.h"
+
+bool str_to_int(char *str, int *converted_int)
+{
+    char *endptr;
+    errno = 0; // reset error number
+    long long_var = strtol(str, &endptr, 10);
+    if (errno == ERANGE)
+    {
+        // out of range for a long
+        return false;
+    }
+    if (endptr == str)
+    {
+        // no character was read
+        return false;
+    }
+    if (*endptr && *endptr != '\n')
+    {
+        // *endptr is neither end of string nor newline,
+        // so we didn't convert the *whole* input
+        return false;
+    }
+    if (long_var > INT_MAX || long_var < INT_MIN)
+    {
+        // result will not fit in an int
+        return false;
+    }
+
+    // write result through the pointer passed
+    *converted_int = (int)long_var;
+    return true;
+}
+
+void remove_spaces(char *str_trimmed, const char *str_untrimmed)
+{
+    while (*str_untrimmed != '\0')
+    {
+        if (!isspace(*str_untrimmed))
+        {
+            *str_trimmed = *str_untrimmed;
+            str_trimmed++;
+        }
+        str_untrimmed++;
+    }
+    *str_trimmed = '\0';
+}
---
+++ helpers.h
@@ -0,0 +1,9 @@
+#ifndef HELPERS_H
+#define HELPERS_H
+
+#include <stdbool.h>
+
+bool str_to_int(char *str, int *converted_int);
+void remove_spaces(char *str_trimmed, const char *str_untrimmed);
+
+#endif
