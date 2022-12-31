12f497e973168194dcce1be56173b20a7f879174
Ahmed Ayman <ahmedayman4a77@gmail.com>
2022-12-31 02:23:31 +0200

    Add whitespace trimming

--- helpers.c
+++ helpers.c
@@ -3,6 +3,7 @@
 #include <errno.h>
 #include <limits.h>
 #include <ctype.h>
+#include <string.h>
 #include "helpers.h"
 
 bool str_to_int(char *str, int *converted_int)
@@ -49,4 +50,29 @@ void remove_spaces(char *str_trimmed, const char *str_untrimmed)
         str_untrimmed++;
     }
     *str_trimmed = '\0';
+}
+
+// Note: This function returns a pointer to a substring of the original string.
+// If the given string was allocated dynamically, the caller must not overwrite
+// that pointer with the returned value, since the original pointer must be
+// deallocated using the same allocator with which it was allocated.  The return
+// value must NOT be deallocated using free() etc.
+char *trim_whitespace(char *str)
+{
+  char *end;
+
+  // Trim leading space
+  while(isspace((unsigned char)*str)) str++;
+
+  if(*str == 0)  // All spaces?
+    return str;
+
+  // Trim trailing space
+  end = str + strlen(str) - 1;
+  while(end > str && isspace((unsigned char)*end)) end--;
+
+  // Write new null terminator character
+  end[1] = '\0';
+
+  return str;
 }
